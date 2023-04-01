a574ff6d52177786eda1bc5d7dda9f4af059d8f9
michael-horansky <michael.horansky@gmail.com>
2023-04-01 15:23:28 +0200

    basic classes initialized

--- heuristic_evaluation/city_model_class.py
+++ heuristic_evaluation/city_model_class.py
@@ -10,12 +10,39 @@ It may be used, modified, shared, or built upon.
 All derivative work must be distributed under the same license.
 """
 
+from infrastructure_class import *
 
 
 
-
-
-
+class CityModel():
+    
+    # ---------------- constructors, destructors, descriptors ----------------------
+    
+    def __init__(self):
+        
+        self.intersections = []
+        self.roads = []
+        self.buildings = []
+        self.buildings_by_type = {service: [] for service in building_types}
+    
+    def add_intersection(self, intersection):
+        
+        self.intersections.append(intersection)
+    
+    def add_road(self, intersections):
+        new_road = Road(intersections)
+        self.roads.append(new_road)
+        for intersection in intersections:
+            intersection.add_road(new_road)
+    
+    def add_building(self, building):
+        self.buildings.append(building)
+        try:
+            self.buildings_by_type[building.service].append(building)
+            return(self)
+        except KeyError:
+            print("building service not recognised")
+            return(-1)
 
 
 
--- heuristic_evaluation/infrastructure_classes.py
+++ heuristic_evaluation/infrastructure_classes.py
@@ -6,6 +6,7 @@ classfile for HackKosice 2023
 road: represents a road, a walkway, a cycle path, a bus connection etc
 intersection: lightweight class that represents an intersection in the
     graph representation of infrastructure
+bulding: represents a building or a facility
 ----------------------------
 This software is distributed under the GNU General Public License (GPL-3.0-or-later).
 It may be used, modified, shared, or built upon.
@@ -13,4 +14,50 @@ All derivative work must be distributed under the same license.
 """
 
 
+# a list of the modes of transportation the model considers
+modes_of_transportation = ['walk', 'cycle', 'car']
 
+# a list of building types
+building_types = ['residential', 'convenience', 'pharmacy', 'jozko vajda']
+
+class Road():
+    
+    # ---------------- constructors, destructors, descriptors ----------------------
+    
+    def __init__(self, connected_intersections, transportation_lengths = {}):
+        self.connected_intersections = connected_intersections
+        
+        self.lengths = transportation_lengths
+
+
+class Intersection():
+    
+    # ---------------- constructors, destructors, descriptors ----------------------
+    
+    def __init__(self, location):
+        # location is always a list [longitude, latitude]
+        self.location = location
+        
+        self.roads = []
+    
+    # ---------------------- parameter-access functions ----------------------------
+    
+    def add_road(self, road):
+        self.roads.append(road)
+
+
+class Building():
+    
+    # ---------------- constructors, destructors, descriptors ----------------------
+    
+    def __init__(self, location, service, surface_area, parameters):
+        # 'service' encodes what type of building this is
+        # 'residential', 'grocery', 'pharmacy'...
+        # 'parameters' is a list of parameters specific to that specific building type
+        
+        self.location = location
+        self.service = service
+        self.surface_area = surface_area
+        self.parameters = parameters
+
+    
