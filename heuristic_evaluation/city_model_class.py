4617475e3eced94e2ae8b10db7d8c584405f10b0
michael-horansky <michael.horansky@gmail.com>
2023-04-01 13:56:32 +0200

    initialized the heuristic algorithm class files

---
+++ heuristic_evaluation/city_model_class.py
@@ -0,0 +1,21 @@
+"""
+created on: 2023-04-01
+author:     michal horansky
+----------------------------
+classfile for HackKosice 2023
+city_model: encodes the abstract city map model, provides heuristic evaluators for relevant parameters
+----------------------------
+This software is distributed under the GNU General Public License (GPL-3.0-or-later).
+It may be used, modified, shared, or built upon.
+All derivative work must be distributed under the same license.
+"""
+
+
+
+
+
+
+
+
+
+
---
+++ heuristic_evaluation/infrastructure_classes.py
@@ -0,0 +1,16 @@
+"""
+created on: 2023-04-01
+author:     michal horansky
+----------------------------
+classfile for HackKosice 2023
+road: represents a road, a walkway, a cycle path, a bus connection etc
+intersection: lightweight class that represents an intersection in the
+    graph representation of infrastructure
+----------------------------
+This software is distributed under the GNU General Public License (GPL-3.0-or-later).
+It may be used, modified, shared, or built upon.
+All derivative work must be distributed under the same license.
+"""
+
+
+
